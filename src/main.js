421c0d0cb25022d6ebe2d1b46b6a60b5f21d9024
royster <1172201059@qq.com>
2023-04-14 21:26:37 +0800

    main.js

--- src/main.js
+++ src/main.js
@@ -1,6 +1,15 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import { createApp } from "vue"
+import App from "./App.vue"
+import router from "./router"
+import store from "./store"
+// 导入初始化样式
+import "@/styles/index.scss"
+// 导入svgIcon
+import installIcons from "@/icons/index"
 
-createApp(App).use(store).use(router).mount('#app')
+// 导入路由鉴权
+import "./permission"
+
+const app = createApp(App)
+installIcons(app)
+app.use(store).use(router).mount("#app")
