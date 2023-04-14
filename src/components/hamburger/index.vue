f75e69db6c2a7c0c72806fb3306d80c6c11a6015
royster <1172201059@qq.com>
2023-04-14 21:28:21 +0800

    控制侧边栏按钮组件

---
+++ src/components/hamburger/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="hamburger-container" @click="toggleClick">
+    <svg-icon class="hamburger" :icon="icon"></svg-icon>
+  </div>
+</template>
+
+<script setup>
+import { computed } from "vue"
+import { useStore } from "vuex"
+
+const store = useStore()
+const toggleClick = () => {
+  store.commit("app/triggerSiderOpened")
+}
+const icon = computed(() => {
+  return store.getters.sidebarOpened ? "hamburger-opened" : "hamburger-closed"
+})
+</script>
+
+<style scoped lang="scss">
+.hamburger-container {
+  padding: 0 16px;
+  .hamburger {
+    display: inline-block;
+    vertical-align: middle;
+    width: 20px;
+    height: 20px;
+  }
+}
+</style>
