8f39572fe21e223b788ab53eb19761096772e54a
royster <1172201059@qq.com>
2023-04-14 21:29:29 +0800

    路由配置

--- src/router/index.js
+++ src/router/index.js
@@ -1,25 +1,148 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-
-const routes = [
+import { createRouter, createWebHistory } from "vue-router"
+import home from "@/views/home/Home.vue"
+/**
+ * 私有路由表
+ */
+const privateRoutes = [
   {
-    path: '/',
-    name: 'home',
-    component: HomeView
+    path: "/user",
+    component: home,
+    redirect: "/user/manage",
+    meta: {
+      title: "user",
+      icon: "personnel",
+    },
+    children: [
+      {
+        path: "/user/manage",
+        name: "userManage",
+        component: () => import("@/views/user-manage/index.vue"),
+        meta: {
+          title: "userManage",
+          icon: "personnel-manage",
+        },
+      },
+      {
+        path: "/user/role",
+        name: "userRole",
+        component: () => import("@/views/role-list/index.vue"),
+        meta: {
+          title: "roleList",
+          icon: "role",
+        },
+      },
+      {
+        path: "/user/permission",
+        name: "userPermission",
+        component: () => import("@/views/permission-list/index.vue"),
+        meta: {
+          title: "permissionList",
+          icon: "permission ",
+        },
+      },
+      {
+        path: "/user/info/:id",
+        name: "userInfo",
+        component: () => import("@/views/user-info/index.vue"),
+        meta: {
+          title: "userInfo",
+        },
+      },
+      {
+        path: "/user/import",
+        name: "import",
+        component: () => import("@/views/import/index.vue"),
+        meta: {
+          title: "excelImport",
+        },
+      },
+    ],
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
+    path: "/article",
+    component: home,
+    redirect: "/article/ranking",
+    meta: { title: "article", icon: "article" },
+    children: [
+      {
+        path: "/article/ranking",
+        component: () => import("@/views/article-ranking/index.vue"),
+        meta: {
+          title: "articleRanking",
+          icon: "article-ranking",
+        },
+      },
+      {
+        path: "/article/:id",
+        component: () => import("@/views/article-detail/index.vue"),
+        meta: {
+          title: "articleDetail",
+        },
+      },
+      {
+        path: "/article/create",
+        name: "articleCreate",
+        component: () => import("@/views/article-create/index.vue"),
+        meta: {
+          title: "articleDetail",
+          icon: "article-create",
+        },
+      },
+      {
+        path: "/article/editor/:id",
+        name: "articleEditor",
+        component: () => import("@/views/article-create/index.vue"),
+        meta: {
+          title: "articleEditor",
+        },
+      },
+    ],
+  },
+]
+
+/**
+ * 公开路由表
+ */
+const publicRoutes = [
+  {
+    path: "/login",
+    name: "login",
+    component: () => import("@/views/login/Login.vue"),
+  },
+  {
+    path: "/",
+    redirect: "/profile",
+    component: () => import("@/views/home/Home.vue"),
+    children: [
+      // 个人中心
+      {
+        path: "/profile",
+        name: "profile",
+        component: () => import("@/views/profile/index.vue"),
+        meta: {
+          title: "profile",
+          icon: "personnel-info",
+        },
+      },
+      // 404
+      {
+        path: "/404",
+        name: "404",
+        component: () => import("@/views/error-page/404.vue"),
+      },
+      // 401
+      {
+        path: "/401",
+        name: "401",
+        component: () => import("@/views/error-page/401.vue"),
+      },
+    ],
+  },
 ]
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  routes
+  history: createWebHistory(),
+  routes: [...publicRoutes, ...privateRoutes],
 })
 
 export default router
