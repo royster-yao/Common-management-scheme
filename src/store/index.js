60d6ad5ff5f4e77c952f174ffad546329a45771b
royster <1172201059@qq.com>
2023-04-14 21:30:09 +0800

    vuex配置

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,12 @@
-import { createStore } from 'vuex'
+import { createStore } from "vuex"
+import user from "./modules/user"
+import getters from "./getters"
+import app from "./modules/app"
 
 export default createStore({
-  state: {
-  },
-  getters: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
+  getters,
   modules: {
-  }
+    user,
+    app,
+  },
 })
